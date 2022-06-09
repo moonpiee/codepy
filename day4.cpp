29921f1a4abaf8a93401008170f6763a5c63cd0c
chand-06 <sparklinthefire@gmail.com>
2022-06-09 12:25:23 +0530

    day4

--- day4.cpp
+++ day4.cpp
@@ -1,13 +1,35 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+
 int main(){
-    int t;
+    ll n=300000; 
+    bool arr[n+1];
+    memset(arr,true,sizeof(arr));
+    for(ll i=2;i<=sqrt(n);i++){
+        if(arr[i]==true){
+            for(ll j=i*i;j<=n;j=j+i){
+                arr[j]=false;
+            }
+        }
+    }
+    arr[0]=false,arr[1]=false;
+    ll t;
     cin>>t;
     while(t--){
-        int h,m;
-        cin>>h>>m;
-        int rem=(23-h)*60+(60-m);
-        cout<<rem<<endl;
+        ll a,b;
+        cin>>a>>b; 
+        ll c=0;
+        for(ll i=a;i<=b;i++){
+            if(arr[i]==true){
+                c++;
+                cout<<i<<endl;
+            }
+        }
+        cout<<c<<endl;
     }
+   
     return 0;
 }
