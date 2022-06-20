e959c63405ffb1910855d0fd22441eef93eb5bad
chand-06 <sparklinthefire@gmail.com>
2022-06-20 12:39:53 +0530

    searching more files added

---
+++ Lost.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<vector>
+using namespace std;
+#define ll long long
+//think of memory optimisation
+ll binarysearch(ll p[],ll n, ll k){
+    
+    ll l=0,h=n-1,mid;
+    while(l<=h){
+         mid=h-(h-l)/2;
+        if(p[mid]==k){
+            return mid;
+        }
+        else if(p[mid]<k){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+    }
+    return -1;
+   
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ll n,q;
+    
+    cin>>n;
+    ll p[n];
+    for(ll i=0;i<n;i++){
+        cin>>p[i];
+    }
+    cin>>q;
+    ll a[q];
+    for(ll i=0;i<q;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<q;i++){
+        cout<<binarysearch(p,n,a[i])<<endl;
+    }
+  
+    return 0;
+}
--- amrLargearr.cpp
+++ amrLargearr.cpp
@@ -5,19 +5,7 @@
 using namespace std;
 #define ll long long
 //think of memory optimisation
-ll fun(ll p[],ll n){
-    unordered_map<ll,ll> m;
-    for(ll i=0;i<n;i++){
-        m[p[i]]++;
-    }
-    for(auto x:m){
-        if(x.second%2!=0){
-            return x.first;
-        }
-    }
-    return -1;
-   
-}
+
 
 int main(){
     #ifndef ONLINE_JUDGE
@@ -25,12 +13,67 @@ int main(){
     freopen("output.txt","w",stdout);
     #endif
     ll n;
-    string s,t;
+    
     cin>>n;
     ll p[n];
+    unordered_map<ll,ll> m;
+    
+    // for(int i=0;i<n;i++){
+    //     freq[i]=0;
+    // }
     for(ll i=0;i<n;i++){
         cin>>p[i];
+        //freq[p[i]]++;
     }
-    cout<<fun(p,n);
-    return 0;
+    for(ll i=0;i<n;i++){
+        m[p[i]]++;
+    }
+    ll max_el=INT_MIN;
+    for(auto x:m){
+        max_el=max(max_el,x.second);
+    }
+    ll f=-1,s=-1;
+    bool found;
+    vector <vector<ll>> v;
+    ll minseg=INT_MAX;
+    // for(auto x:m){
+    //     if(x.second==max_el)
+    //     cout<<x.first<<endl;
+    // }
+    for(auto x:m){
+        bool found=false;
+        // cout<<"x: "<<x.first<<endl;
+        if(x.second==max_el){
+           for(int i=0;i<n;i++){
+            if(x.first==p[i]){
+                //f=p[i];
+                for(int j=n;j>=i+1;j--){
+                    if(x.first==p[j]){
+                        //s=p[j];
+                        if(f==-1 || j-i<minseg){
+                            f=i;
+                            s=j;
+                            // cout<<f<<endl;
+                            // cout<<s<<endl;
+                            minseg=(s-f);
+
+                        }
+                         found=true;
+                         break;
+                    }
+                }
+            }
+            if(found) break;
+            
+           }
+        }
+        
+    }
+    if(f!=-1&&s!=-1)
+        cout<<f+1<<" "<<s+1<<endl;
+    else
+        cout<<1<<" "<<1<<endl; 
+   
+
+   return 0;
 }
