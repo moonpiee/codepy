21bb88dbc6cc7cc3ba958a44bb14be9b6fc543df
moonpiee <sparklinthefire@gmail.com>
2022-07-18 14:06:04 +0530

    nj

--- sushi.cpp
+++ sushi.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    freopen("output.txt","w",stdout);;;
     #endif
     ll n;
     vector<int>v;
